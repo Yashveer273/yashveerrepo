1badeb0c138582ad3c03a6eb361cfc8e19145c17
Yashveer273 <Yashveerverma266@gmail.com>
2023-07-08 16:26:31 +0530

    update submodule

---
+++ build/precache-manifest.4f29a2ef5adb49f5226350b9fbc94f34.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = [
+  {
+    "revision": "9db7a872687e4e9d547c",
+    "url": "/my-app/static/css/main.fbf80aa6.chunk.css"
+  },
+  {
+    "revision": "9db7a872687e4e9d547c",
+    "url": "/my-app/static/js/main.9db7a872.chunk.js"
+  },
+  {
+    "revision": "0ac1ce4c7e9ee29517a9",
+    "url": "/my-app/static/js/1.0ac1ce4c.chunk.js"
+  },
+  {
+    "revision": "0b1b663406f272f4b7c9",
+    "url": "/my-app/static/css/2.c8afe8b0.chunk.css"
+  },
+  {
+    "revision": "0b1b663406f272f4b7c9",
+    "url": "/my-app/static/js/2.0b1b6634.chunk.js"
+  },
+  {
+    "revision": "55d803d5f94235082e70",
+    "url": "/my-app/static/js/runtime~main.55d803d5.js"
+  },
+  {
+    "revision": "5761a4e54dc2c0f3954d8d5cfbd3881a",
+    "url": "/my-app/static/media/21.05.27-Mint-Subscription-smaller.5761a4e5.webp"
+  },
+  {
+    "revision": "2e2f8e6f17f48774911e2cda94ae9221",
+    "url": "/my-app/static/media/bite.2e2f8e6f.jpg"
+  },
+  {
+    "revision": "b992e6c18c6c5b1c4bd2cb53f5927d29",
+    "url": "/my-app/static/media/c1.b992e6c1.webp"
+  },
+  {
+    "revision": "92d3b39ba04ce5d79614c66cd294d0a4",
+    "url": "/my-app/static/media/container3.92d3b39b.webp"
+  },
+  {
+    "revision": "18c95b95940d9c3af2f075a8e1106e2d",
+    "url": "/my-app/static/media/container4.18c95b95.webp"
+  },
+  {
+    "revision": "ff31ce1f8669e227d2ef2633c7f02e98",
+    "url": "/my-app/static/media/bottel.ff31ce1f.webp"
+  },
+  {
+    "revision": "d7db26073e6e9e17a36663a93758d6f8",
+    "url": "/my-app/static/media/brush.d7db2607.webp"
+  },
+  {
+    "revision": "1866bcaf5d98257756defb43af5f08f6",
+    "url": "/my-app/static/media/daily habits kit.1866bcaf.webp"
+  },
+  {
+    "revision": "2cd59ae7dfdc0011901721a3b7d3d2fa",
+    "url": "/my-app/static/media/deodorant.2cd59ae7.webp"
+  },
+  {
+    "revision": "4ef69816dae4686f194c05a76a3de78e",
+    "url": "/my-app/static/media/cutlery.4ef69816.png"
+  },
+  {
+    "revision": "9d8ab2eb70b7cd79c4f4a49f65c769f4",
+    "url": "/my-app/static/media/Pizz.9d8ab2eb.png"
+  },
+  {
+    "revision": "f90ba900dc43958f3d2adb0cc8e5025c",
+    "url": "/my-app/static/media/juice.f90ba900.png"
+  },
+  {
+    "revision": "bd833f016be3ec693a064e9c4fd76a67",
+    "url": "/my-app/static/media/sundae.bd833f01.png"
+  },
+  {
+    "revision": "f6bd28d092c2e5a25526973902f3dea6",
+    "url": "/my-app/static/media/penne.f6bd28d0.png"
+  },
+  {
+    "revision": "c8f9b29ce9d94a274c86e15e5d017e59",
+    "url": "/my-app/static/media/noodles.c8f9b29c.png"
+  },
+  {
+    "revision": "451dcd1ea279f5db743ec8060049b6bd",
+    "url": "/my-app/static/media/c1.1.451dcd1e.webp"
+  },
+  {
+    "revision": "d75f896cf382fa1f56ae2af88db1bc65",
+    "url": "/my-app/static/media/c2.d75f896c.webp"
+  },
+  {
+    "revision": "be72b1b4c23560a1c90928ffb762e6f2",
+    "url": "/my-app/static/media/c.2.1.be72b1b4.webp"
+  },
+  {
+    "revision": "077ad1f66bd522cc7125bf846432b55d",
+    "url": "/my-app/static/media/c3.077ad1f6.webp"
+  },
+  {
+    "revision": "6b3b04c61067939ca0587297678b15b5",
+    "url": "/my-app/static/media/c3.1.6b3b04c6.webp"
+  },
+  {
+    "revision": "90eea61a8f08f6ea14881032aeb349f7",
+    "url": "/my-app/static/media/c4.90eea61a.webp"
+  },
+  {
+    "revision": "b8b11f314383e6f02a9a31cc95935f0c",
+    "url": "/my-app/static/media/c4.1.b8b11f31.webp"
+  },
+  {
+    "revision": "81dfc8fbdedc4b7798da11f966a93ad3",
+    "url": "/my-app/static/media/c5.81dfc8fb.webp"
+  },
+  {
+    "revision": "5113f7c1d3668fe1106ffbed3158bcda",
+    "url": "/my-app/static/media/c5.1.5113f7c1.webp"
+  },
+  {
+    "revision": "0d084339b676cfedeccc4fdbbb468303",
+    "url": "/my-app/static/media/pizzabg2.0d084339.jpg"
+  },
+  {
+    "revision": "46ff2306ead725a688f3c9cc649d16b5",
+    "url": "/my-app/index.html"
+  }
+];
